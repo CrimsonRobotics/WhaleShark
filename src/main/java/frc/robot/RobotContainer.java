0aa97b5dbca02efd35c4850ef3daa94597eb9d4d
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2025-02-19 09:46:04 -0600

    all basic operator buttons have been added into robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,21 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.Operator.BargeScore;
+import frc.robot.commands.Operator.IntakeAlgae;
+import frc.robot.commands.Operator.Resting;
+import frc.robot.commands.Operator.Traveling;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -21,6 +33,37 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Climber climber = new Climber();
+  private final Intake intake = new Intake();
+  private final Elevator elevator = new Elevator();
+  private final Drivetrain drivetrain = new Drivetrain();
+
+  //Creating joysticks
+  private final Joystick l_drive = new Joystick(0);
+  private final Joystick r_drive = new Joystick(1);
+  private final Joystick l_operator = new Joystick(2);
+  private final Joystick r_operator = new Joystick(3);
+  
+  //Creating Joystick Buttons
+
+  //Left Operator Joystick
+  //Button 8
+  private final JoystickButton resting = new JoystickButton(l_operator, 8);
+  //Button 9
+  private final JoystickButton traveling = new JoystickButton(l_operator, 9);
+
+  //Right Operator Joystick
+  //Button 12
+  private final JoystickButton coral_intake = new JoystickButton(r_operator, 12);
+  //Button 13
+  private final JoystickButton ground_intake = new JoystickButton(r_operator, 13);
+  //Button 14
+  private final JoystickButton low_reef_intake = new JoystickButton(r_operator, 14);
+  //Button 15
+  private final JoystickButton high_reef_intake = new JoystickButton(r_operator, 15);
+  //Button 16
+  private final JoystickButton barge_score = new JoystickButton(r_operator, 16);
+
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -49,6 +92,18 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+
+    //configuring buttons
+    //left operator buttons
+    resting.onTrue(new InstantCommand(() -> elevator.setDefaultCommand(new Resting(elevator, intake))));
+    traveling.onTrue(new InstantCommand(() -> elevator.setDefaultCommand(new Traveling(elevator, intake))));
+    //right operator buttons
+    coral_intake.whileTrue(new IntakeAlgae(elevator, intake, Constants.elevator.coral));
+    ground_intake.whileTrue(new IntakeAlgae(elevator, intake, Constants.elevator.ground));
+    low_reef_intake.whileTrue(new IntakeAlgae(elevator, intake, Constants.elevator.low_reef));
+    high_reef_intake.whileTrue(new IntakeAlgae(elevator, intake, Constants.elevator.high_reef));
+    barge_score.whileTrue(new BargeScore(elevator, intake));
   }
 
   /**
