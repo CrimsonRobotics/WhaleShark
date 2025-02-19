cfed09bebdc3dd49835cafc473f2c541be43087c
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2025-02-19 08:24:04 -0600

    Commented in intake subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -24,22 +24,45 @@ public class Intake extends SubsystemBase {
   SparkFlexConfig hold_config;
   SparkFlexConfig intake_config;
   public Intake() {
+    //creates the solenoid for the intake going in and out
+    //The pneumatics module type is REVPH(REV Pneumatics Hub is the pneumatics controller made by REV Robotics)
+    //the channels are plugged into 0 and 1, but which is which needs to be checked
     solenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
+    //The intake motor is a NEO Vortex which is beinc controlled by a spark flex controller
+    //neo vortex is brushless
     intake_motor = new SparkFlex(Constants.intake.motor_id, MotorType.kBrushless);
 
+    //creates the spark flex configs for the intake motor
+    //there are two configs because we will need two different current limits
+    //the current limit blocks how many electrons can go through the motor at one time, which effectively controls the motors resistance
+    //when holding a ball we will want a low current limit so that we can spin in and hold the ball without destroying it
     hold_config = new SparkFlexConfig();
+    //when intaking and shooting we will want a higher current limit so that we can pull the ball in
     intake_config = new SparkFlexConfig();
 
+    //configures the spark flex motor controller
     intake_config
+      //sets the idle mode to brake
       .idleMode(IdleMode.kBrake)
+      //sets the motor to not be inverted
       .inverted(false)
-      .smartCurrentLimit(Constants.intake.intake_current_limit);
+      //sets the current limit to the intake current limit from Constants
+      .smartCurrentLimit(Constants.intake.intake_current_limit)
+      //will scale all .run() on the motor controller as if the battery has full charge
+      .voltageCompensation(12);
 
+    //configures the spark flex motor controller
     hold_config
+      //sets the idle mode to brake
       .idleMode(IdleMode.kBrake)
+      //sets the motor to not be inverted
       .inverted(false)
-      .smartCurrentLimit(Constants.intake.hold_current_limit);
+      //sets the current limit to the hold current limit from Constants
+      .smartCurrentLimit(Constants.intake.hold_current_limit)
+      //will scale all .run() on the motor controller as if the battery has full charge
+      .voltageCompensation(12);
 
+    //configures the spark flex motor controller with the intake config
     intake_motor.configure(intake_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
   }
@@ -51,6 +74,8 @@ public class Intake extends SubsystemBase {
 
   //changes the configuration of the spark flex motor controller based on if the motor is going to be holding algae or intaking algae
   public void configure(Constants.intake.state state) {
+    //switch is a quick if else statement
+    //which ever case is true will defnine which config is used
     switch (state) {
       case HOLD:
         intake_motor.configure(hold_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
