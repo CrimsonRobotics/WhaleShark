9ba758b33d4089984ee99bb4762d4bb8a6d2f9dd
kvanklei <kaylavk@yahoo.com>
2025-02-28 00:40:09 -0600

    added the pruntoposition command for going to barge

---
+++ src/main/java/frc/robot/commands/Elevator/PRunToPosition.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Elevator;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class PRunToPosition extends Command {
+  /** Creates a new PRunToPosition. */
+  Elevator elevator;
+  double position;
+  Joystick joystick;
+  public PRunToPosition(Elevator elevator, double position, Joystick joystick) {
+    this.elevator = elevator;
+    this.position = position;
+    this.joystick = joystick;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.elevator);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    this.elevator.p_to_position(this.position, this.joystick.getX());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    this.elevator.run(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
