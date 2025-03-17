63c70cbcf6d69d66f5aa1ca0ff46a2d8d7b77396
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2025-03-17 11:10:53 -0500

    removed elevator from resting command

--- src/main/java/frc/robot/commands/Operator/Resting.java
+++ src/main/java/frc/robot/commands/Operator/Resting.java
@@ -7,45 +7,34 @@ package frc.robot.commands.Operator;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
-import frc.robot.commands.Elevator.HoldPosition;
 import frc.robot.commands.Intake.Extend;
 import frc.robot.commands.Intake.Rest;
 import frc.robot.commands.Intake.RunRoller;
-import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class Resting extends ParallelCommandGroup {
+public class Resting extends SequentialCommandGroup {
   /** Creates a new Rest. */
-  Elevator elevator;
   Intake intake;
-  public Resting(Elevator elevator, Intake intake) {
+  public Resting(Intake intake) {
     //sets the paramaters to local instance variables
-    this.elevator = elevator;
     this.intake = intake;
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
 
     //these commands will both happen at the same time
     addCommands(
-      //the elevator will hold positiot at rest height
-      Commands.sequence(
-        new WaitCommand(1)
-        //new HoldPosition(this.elevator, Constants.elevator.ground)
+      new Extend(this.intake),
+      Commands.race(
+        new WaitCommand(1),
+        new RunRoller(this.intake, Constants.intake.intake_speed)
       ),
-      //the intake will go into rest state
-      Commands.sequence(
-        new Extend(this.intake),
-        Commands.race(
-          new WaitCommand(1),
-          new RunRoller(this.intake, Constants.intake.intake_speed)
-        ),
-        new Rest(this.intake)
-      )
+      new Rest(this.intake)
     );
   }
 }
